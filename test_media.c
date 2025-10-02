356e16f7033ef6b31f527514d053cd20a4390779
C4LUS <lucas1.martin@epitech.eu>
2025-10-02 15:21:19 +0400

    [ADD] screenmiroring with gstreamer lib

---
+++ test_media.c
@@ -0,0 +1,48 @@
+#include <gst/gst.h>
+
+int main(int argc, char *argv[])
+{
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
+
+    /* Initialize GStreamer */
+    gst_init(&argc, &argv);
+
+    /* Pipeline de test : génère un motif de test vidéo et l’affiche */
+    pipeline = gst_parse_launch("gst-launch-1.0 ximagesrc ! videoconvert ! autovideosink", NULL);
+    if (!pipeline) {
+        g_printerr("Failed to create pipeline\n");
+        return -1;
+    }
+
+    /* Start playing */
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    /* Wait until error or EOS */
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                     GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+    if (msg) {
+        if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
+            GError *err;
+            gchar *dbg;
+            gst_message_parse_error(msg, &err, &dbg);
+            g_printerr("Error: %s\nDebug info: %s\n",
+                       err->message, dbg ? dbg : "none");
+            g_error_free(err);
+            g_free(dbg);
+        } else {
+            g_print("End-Of-Stream reached.\n");
+        }
+        gst_message_unref(msg);
+    }
+
+    /* Cleanup */
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+
+    return 0;
+}
